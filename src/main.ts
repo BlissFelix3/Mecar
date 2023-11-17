381d68ab024953196194689bb8fc5cba16833533
Bliss <blissfelix18@gmail.com>
2023-11-17 11:27:38 +0100

    chore: main config

---
+++ src/main.ts
@@ -0,0 +1,38 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { config } from 'dotenv';
+import { ConfigService } from '@nestjs/config';
+import { INestApplication } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import * as cookieParser from 'cookie-parser';
+config();
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  app.use(cookieParser());
+
+  const appHost = 'http://localhost:3000';
+  const configService = app.get<ConfigService>(ConfigService);
+  const environment = configService.get('environment');
+
+  const initSwagger = (app: INestApplication, serverUrl: string) => {
+    const config = new DocumentBuilder()
+      .setTitle('Mecar')
+      .setDescription('Mecar Web Application')
+      .addServer(serverUrl)
+      .setVersion('1.0')
+      .addBearerAuth()
+      .build();
+
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('api', app, document);
+  };
+
+  if (environment !== 'production') {
+    initSwagger(app, appHost);
+  }
+
+  await app.listen(3000);
+}
+bootstrap();
