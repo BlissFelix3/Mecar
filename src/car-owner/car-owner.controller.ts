02ee7da2f5ccf34988f4c1b33f984f14275f9590
Bliss <blissfelix18@gmail.com>
2023-11-17 11:31:17 +0100

    feat: car-owner endpoint

---
+++ src/car-owner/car-owner.controller.ts
@@ -0,0 +1,19 @@
+import { Controller, Post, Request, Body } from '@nestjs/common';
+import { CarOwnerService } from './car-owner.service';
+import { CreateCarOwnerDto } from './dto/create-car-owner.dto';
+import { CarOwner } from './entities/car-owner.entity';
+import { Roles } from 'src/shared/decorators/roles.decorator';
+import { UserRole } from 'src/shared/enums';
+
+@Controller('car-owners')
+export class CarOwnerController {
+  constructor(private readonly carOwnerService: CarOwnerService) {}
+
+  @Roles(UserRole.CAR_OWNER)
+  @Post('signup')
+  async createCarOwner(
+    @Body() createCarOwnerDto: CreateCarOwnerDto,
+  ): Promise<CarOwner> {
+    return this.carOwnerService.createCarOwner(createCarOwnerDto);
+  }
+}
