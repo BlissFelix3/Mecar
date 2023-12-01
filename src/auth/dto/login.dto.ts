7d74d69dac6c14ab492b256cc4db7787238e7703
Bliss <blissfelix18@gmail.com>
2023-12-01 14:41:40 +0100

    refactor: improve code

--- src/auth/dto/login.dto.ts
+++ src/auth/dto/login.dto.ts
@@ -1,11 +1,6 @@
 import { IsNotEmpty, IsString } from 'class-validator';
 
 export class LoginDto {
-  /* Login with either email or phone number */
-  // @IsNotEmpty()
-  // @IsString()
-  // identifier: string;
-
   @IsNotEmpty()
   @IsString()
   emailOrPhone: string;
