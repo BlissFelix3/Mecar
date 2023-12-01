0324334a7781e01f0a3a8acdc37941069563eff2
Bliss <blissfelix18@gmail.com>
2023-12-01 14:44:41 +0100

    feat: add reset mail

--- src/mail/mail.module.ts
+++ src/mail/mail.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { MailService } from './mail.service';
 import { MailController } from './mail.controller';
+import { ConfigService, ConfigModule } from '@nestjs/config';
 
 @Module({
-  providers: [MailService],
+  imports: [ConfigModule],
+  providers: [MailService, ConfigService],
   controllers: [MailController],
 })
 export class MailModule {}
--- src/mail/mail.service.ts
+++ src/mail/mail.service.ts
@@ -1,4 +1,61 @@
+// email.service.ts
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import * as nodemailer from 'nodemailer';
+import * as hbs from 'nodemailer-express-handlebars';
 
 @Injectable()
-export class MailService {}
+export class MailService {
+  private transporter;
+
+  constructor(private configService: ConfigService) {
+    this.transporter = nodemailer.createTransport({
+      host: this.configService.get('MAIL_HOST'),
+      port: this.configService.get('MAIL_PORT'),
+      secure: true,
+      auth: {
+        user: this.configService.get('MAIL_USER'),
+        pass: this.configService.get('MAIL_PASSWORD'),
+      },
+      socketTimeout: 30000,
+    });
+
+    this.setupTemplates();
+  }
+
+  private setupTemplates() {
+    const handlebarOptions = {
+      viewEngine: {
+        extname: '.hbs',
+        layoutsDir: 'src/mail/public',
+        defaultLayout: 'reset-password',
+      },
+      viewPath: 'src/mail/public',
+      extName: '.hbs',
+    };
+
+    this.transporter.use('compile', hbs(handlebarOptions));
+  }
+
+  async sendResetPasswordEmail(
+    email: string,
+    resetLink: string,
+  ): Promise<void> {
+    try {
+      const mailOptions = {
+        from: '"MECAR" <no-reply@mailer.com>',
+        to: email,
+        subject: 'Reset Your Password',
+        template: 'reset-password',
+        context: {
+          resetLink,
+        },
+      };
+
+      await this.transporter.sendMail(mailOptions);
+    } catch (error: any) {
+      console.error('Error sending reset password email:', error.message);
+      throw error;
+    }
+  }
+}
