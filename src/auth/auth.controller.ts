4745f8d02c7a1bef90f47569e41d160cf3c410f3
Bliss <blissfelix18@gmail.com>
2023-12-01 14:39:34 +0100

    refactor: remove comments

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -6,21 +6,19 @@ import {
   UseInterceptors,
   ClassSerializerInterceptor,
   Res,
-  // Patch,
-  Param,
-  // Request,
-  // Response,
   HttpException,
   HttpStatus,
   Req,
+  Param,
   Get,
+  Patch,
+  Headers,
 } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { SignupDto } from './dto/signup.dto';
 import { LoginDto } from './dto/login.dto';
-// import { RolesGuard } from './guards/roles.guard';
+import { RolesGuard } from './guards/roles.guard';
 import { LocalAuthGuard } from './guards/local-auth.guard';
-// import { JwtAuthGuard } from './guards/jwt-auth.guard';
 import { UserRole } from 'src/shared/enums';
 import { Roles } from 'src/shared/decorators/roles.decorator';
 import { BadRequestException, UnauthorizedException } from '@nestjs/common';
@@ -35,6 +33,7 @@ import { AuthGuard } from '@nestjs/passport';
 import { JwtAuthGuard } from './guards/jwt-auth.guard';
 import { ApiBearerAuth } from '@nestjs/swagger';
 import { VerifyOtpDto } from './dto/verify-otp.dto';
+import { ChangePasswordDto } from './dto/changePassword.dto';
 
 @Controller('auth')
 @UseInterceptors(ClassSerializerInterceptor)
@@ -48,6 +47,7 @@ export class AuthController {
 
   @Roles(UserRole.USER)
   @Post('signup')
+  // @UseGuards(RolesGuard)
   async signup(
     @Body() signupDto: SignupDto,
   ): Promise<{ user: User; otpMessage: string }> {
@@ -61,6 +61,102 @@ export class AuthController {
     @Res({ passthrough: true }) res: Response,
   ): Promise<any> {
     return this.authService.login(loginDto, res);
+  }
+
+  @UseGuards(AuthGuard('jwt'))
+  @Post('enable-2fa')
+  async enableTwoFactorAuthentication(
+    @GetUser() user: User,
+    @Body('phoneNumber') phoneNumber: string,
+  ): Promise<{ message: string }> {
+    try {
+      const result = await this.authService.enableTwoFactorAuthentication(
+        user,
+        phoneNumber,
+      );
+      return { message: result.message };
+    } catch (error) {
+      return { message: 'Failed to enable two-factor authentication' };
+    }
+  }
+
+  @UseGuards(AuthGuard('jwt'))
+  @Post('verify-2fa')
+  async verifyTwoFactorAuthentication(
+    @Headers('user-id') userId: string,
+    @Body('code') code: string,
+    @Body('verificationSid') verificationSid: string,
+  ): Promise<{ message: string }> {
+    await this.authService.verifyTwoFactorAuthentication(
+      userId,
+      code,
+      verificationSid,
+    );
+    return { message: 'Two-factor authentication verified successfully' };
+  }
+
+  @UseGuards(AuthGuard('jwt'))
+  @Post('disable-2fa')
+  async disableTwoFactorAuthentication(
+    @Headers('user-id') userId: string,
+  ): Promise<{ message: string }> {
+    await this.authService.disableTwoFactorAuthentication(userId);
+    return { message: 'Two-Factor authentication disabled successfully' };
+  }
+
+  @UseGuards(AuthGuard('jwt'))
+  @Post('resend-otp')
+  async resendOtp(
+    @Headers('phone-number') phoneNumber: string,
+  ): Promise<{ message: string }> {
+    try {
+      return await this.authService.resendOtp(phoneNumber);
+    } catch (error: any) {
+      throw new BadRequestException('Failed to resend OTP: ' + error.message);
+    }
+  }
+
+  @Post('verify-otp')
+  async verifyOtp(
+    @Headers('phone-number') phoneNumber: string,
+    @Body() dto: VerifyOtpDto,
+    @Body('verificationSid') verificationSid: string,
+  ): Promise<{ message: string }> {
+    try {
+      await this.authService.verifyOtp(phoneNumber, dto, verificationSid);
+      return { message: 'OTP verified successfully' };
+    } catch (error: any) {
+      throw new BadRequestException('Failed to verify OTP: ' + error.message);
+    }
+  }
+
+  @Post('send-reset-password-email')
+  async sendResetPasswordEmail(
+    @Body('email') email: string,
+  ): Promise<{ message: string }> {
+    try {
+      await this.authService.sendResetPasswordEmail(email);
+      return { message: 'Reset password email sent successfully' };
+    } catch (error: any) {
+      throw new BadRequestException(
+        'Failed to send reset password email: ' + error.message,
+      );
+    }
+  }
+
+  @Post('reset-password/:token')
+  async resetPasswordWithToken(
+    @Param('token') token: string,
+    @Body('newPassword') newPassword: string,
+    @Body('confirmNewPassword') confirmNewPassword: string,
+  ): Promise<{ message: string }> {
+    await this.authService.resetPasswordWithToken(
+      token,
+      newPassword,
+      confirmNewPassword,
+    );
+
+    return { message: 'Password reset successful' };
   }
 
   @Post('refresh-token')
@@ -112,50 +208,7 @@ export class AuthController {
     }
   }
 
-  @Post('send-otp')
-  async sendOtp(
-    @Body('phoneNumber') phoneNumber: string,
-  ): Promise<{ message: string }> {
-    try {
-      const user = await this.userRepository.findOne({
-        where: { phoneNumber },
-      });
-
-      if (!user) {
-        throw new UnauthorizedException('User not found');
-      }
-
-      await this.twilioService.sendOtp(user);
-      return { message: 'OTP sent successfully' };
-    } catch (error: any) {
-      throw new BadRequestException(
-        'Failed to send OTP. Phone Number is not registered.',
-      );
-    }
-  }
-
-  @Post('verify-otp/:phoneNumber')
-  async verifyOtp(
-    @Param('phoneNumber') phoneNumber: string,
-    @Body() dto: VerifyOtpDto,
-    @Body('verificationSid') verificationSid: string,
-  ): Promise<{ message: string }> {
-    try {
-      const user = await this.userRepository.findOne({
-        where: { phoneNumber },
-      });
-
-      if (!user) {
-        throw new UnauthorizedException('User not found');
-      }
-
-      await this.twilioService.verifyOtp(user, dto, verificationSid);
-      return { message: 'OTP verified successfully' };
-    } catch (error: any) {
-      throw new BadRequestException('Failed to verify OTP: ' + error.message);
-    }
-  }
-
+  @UseGuards(AuthGuard('jwt'))
   @Post('logout')
   async logout(
     @Req() req: Request,
@@ -166,9 +219,20 @@ export class AuthController {
   }
 
   @ApiBearerAuth()
-  @Get('/profile')
-  @UseGuards(JwtAuthGuard)
+  @Get('profile')
+  @UseGuards(JwtAuthGuard, RolesGuard)
   async viewProfile(@GetUser() user: User) {
     return this.authService.viewProfile(user);
+  }
+
+  @UseGuards(AuthGuard('jwt'))
+  @Patch('change-password')
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  async changePassword(
+    @GetUser() user: User,
+    @Body() changePasswordDto: ChangePasswordDto,
+  ): Promise<{ message: string }> {
+    await this.authService.changePassword(changePasswordDto, user);
+    return { message: 'Password changed successfully' };
   }
 }
