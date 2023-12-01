60974654eb7484f7a800ddc3dc45450b8d71094e
Bliss <blissfelix18@gmail.com>
2023-12-01 14:42:00 +0100

    feat: add reset-password

---
+++ src/auth/dto/reset-password.dto.ts
@@ -0,0 +1,25 @@
+import { ApiProperty } from '@nestjs/swagger';
+import {
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  Matches,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
+
+export class ResetPasswordDto {
+  @ApiProperty()
+  @IsOptional()
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(6)
+  @MaxLength(20)
+  newPassword?: string;
+
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsOptional()
+  @IsString()
+  confirmNewPassword?: string;
+}
