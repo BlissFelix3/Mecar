deaa21dbed1f8a4edae48771f33795a43b4393c4
Bliss <blissfelix18@gmail.com>
2024-01-15 16:31:56 +0100

    feat: add orders

--- src/orders/orders.service.ts
+++ src/orders/orders.service.ts
@@ -1,6 +1,3 @@
 import { Injectable } from '@nestjs/common';
-import { Review } from 'src/reviews/entity/review.entity';
-import { InjectRepository } from '@nestjs/typeorm';
-
 @Injectable()
 export class OrdersService {}
