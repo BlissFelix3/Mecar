61d6985ed6eefa0e154afce6ee28f505bdecc208
Bliss <blissfelix18@gmail.com>
2024-01-15 16:19:51 +0100

    style: import more modules

--- src/car-owner/car-owner.module.ts
+++ src/car-owner/car-owner.module.ts
@@ -1,14 +1,26 @@
 import { Module } from '@nestjs/common';
 import { CarOwnerController } from './car-owner.controller';
 import { CarOwnerService } from './car-owner.service';
+import { RequestService } from './services/request-service.service';
+import { GeneralRepair } from './services/entities/general-repair.entity';
+import { MaintenanceRepair } from './services/entities/maintenance-repair.entity';
+import { Cars } from 'src/cars/entities/car.entity';
 import { CarOwner } from './entities/car-owner.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from 'src/users/entities';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([CarOwner, User])],
+  imports: [
+    TypeOrmModule.forFeature([
+      CarOwner,
+      User,
+      GeneralRepair,
+      MaintenanceRepair,
+      Cars,
+    ]),
+  ],
   controllers: [CarOwnerController],
-  providers: [CarOwnerService],
-  exports: [CarOwnerService],
+  providers: [CarOwnerService, RequestService],
+  exports: [CarOwnerService, RequestService],
 })
 export class CarOwnerModule {}
