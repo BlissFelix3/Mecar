1834f0892cffd90cc658dbfaf0554074fd1e9871
Bliss <blissfelix18@gmail.com>
2024-01-15 16:25:37 +0100

    style: update imports

--- src/mechanic/mechanic.controller.ts
+++ src/mechanic/mechanic.controller.ts
@@ -1,22 +1,39 @@
 import {
   Controller,
+  Get,
   Post,
+  Patch,
+  Delete,
   Body,
   UseInterceptors,
   UploadedFiles,
   Headers,
   UseGuards,
+  Param,
 } from '@nestjs/common';
 import { MechanicService } from './mechanic.service';
-import { CreateMechanicDto } from './dto/create-mechanic.dto';
+import { CreateMechanicDto } from './dtos/create-mechanic.dto';
 import { Mechanic } from './entities/mechanic.entity';
 import { FileFieldsInterceptor } from '@nestjs/platform-express';
-import { ApiBody, ApiConsumes } from '@nestjs/swagger';
+import { ApiBody, ApiConsumes, ApiBearerAuth } from '@nestjs/swagger';
 import { RolesGuard } from 'src/auth/guards/roles.guard';
+import { ApiTags } from '@nestjs/swagger';
+import { API_TAGS } from 'src/common/enums';
+import { Roles } from 'src/common/decorators/roles.decorator';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
+import { RenderService } from './services/render-service.service';
+import { UserRole } from 'src/common/enums';
+import { MechanicServiceEntity } from './services/entities/mechanic.service.entity';
+import { AddServiceDto } from './services/dtos/add-service.dto';
+import { UpdateServiceDto } from './services/dtos/update-service.dto';
 
+@ApiTags(API_TAGS.MECHANIC)
 @Controller('mechanics')
 export class MechanicController {
-  constructor(private readonly mechanicService: MechanicService) {}
+  constructor(
+    private readonly mechanicService: MechanicService,
+    private readonly renderService: RenderService,
+  ) {}
 
   @Post('create')
   @UseGuards(RolesGuard)
@@ -44,5 +61,75 @@ export class MechanicController {
       files,
       registrationToken,
     );
+  }
+
+  @Post(':mechanicId/services/create')
+  @ApiBearerAuth()
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles(UserRole.MECHANIC)
+  async addService(
+    @Param('mechanicId') mechanicId: string,
+    @Body() addServiceDto: AddServiceDto,
+  ): Promise<MechanicServiceEntity> {
+    return this.renderService.addService(mechanicId, addServiceDto);
+  }
+
+  @Get(':mechanicId/services')
+  @ApiBearerAuth()
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles(UserRole.MECHANIC)
+  async getServices(
+    @Param('mechanicId') mechanicId: string,
+  ): Promise<MechanicServiceEntity[]> {
+    return this.renderService.getAllServices(mechanicId);
+  }
+
+  @Patch(':mechanicId/services/:serviceId/update')
+  @ApiBearerAuth()
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles(UserRole.MECHANIC)
+  async updateService(
+    @Param('mechanicId') mechanicId: string,
+    @Param('serviceId') serviceId: string,
+    @Body() updateServiceDto: UpdateServiceDto,
+  ): Promise<MechanicServiceEntity> {
+    return this.renderService.updateService(
+      mechanicId,
+      serviceId,
+      updateServiceDto,
+    );
+  }
+
+  @Delete(':mechanicId/services/:serviceId/delete')
+  @ApiBearerAuth()
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles(UserRole.MECHANIC)
+  async deleteService(
+    @Param('mechanicId') mechanicId: string,
+    @Param('serviceId') serviceId: string,
+  ): Promise<{ message: string }> {
+    return this.renderService.deleteService(mechanicId, serviceId);
+  }
+
+  @Patch(':mechanicId/services/:serviceId/pause')
+  @ApiBearerAuth()
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles(UserRole.MECHANIC)
+  async pauseService(
+    @Param('mechanicId') mechanicId: string,
+    @Param('serviceId') serviceId: string,
+  ): Promise<MechanicServiceEntity> {
+    return this.renderService.pauseService(mechanicId, serviceId);
+  }
+
+  @Patch(':mechanicId/services/:serviceId/activate')
+  @ApiBearerAuth()
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles(UserRole.MECHANIC)
+  async activateService(
+    @Param('mechanicId') mechanicId: string,
+    @Param('serviceId') serviceId: string,
+  ): Promise<MechanicServiceEntity> {
+    return this.renderService.activatePausedService(mechanicId, serviceId);
   }
 }
