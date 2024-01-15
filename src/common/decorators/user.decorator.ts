86514d3f879e9cd14dcc1f7939a7f9eaf32138f4
Bliss <blissfelix18@gmail.com>
2024-01-15 16:24:32 +0100

    feat: add user decorator

---
+++ src/common/decorators/user.decorator.ts
@@ -0,0 +1,20 @@
+import { createParamDecorator, ExecutionContext } from '@nestjs/common';
+import { User } from 'src/users/entities';
+
+export const GetUser = createParamDecorator(
+  (_data, ctx: ExecutionContext): User => {
+    const req = ctx.switchToHttp().getRequest();
+    const res = ctx.switchToHttp().getResponse();
+    return { ...req.user, res };
+  },
+);
+
+export const GetQuery = createParamDecorator(
+  (_data, ctx: ExecutionContext): User => {
+    const req = ctx.switchToHttp().getRequest();
+    const res = ctx.switchToHttp().getResponse();
+    return { ...req, res };
+  },
+);
+
+export const GetUrl = GetQuery();
