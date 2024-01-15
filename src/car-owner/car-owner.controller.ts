3a9ec84833672e58bab98c0f51f91624f5de1520
Bliss <blissfelix18@gmail.com>
2024-01-15 15:34:36 +0100

    style: update imports

--- src/car-owner/car-owner.controller.ts
+++ src/car-owner/car-owner.controller.ts
@@ -1,14 +1,28 @@
-import { Controller, Post, Body, Headers } from '@nestjs/common';
+import { Controller, Post, Body, Headers, UseGuards } from '@nestjs/common';
 import { CarOwnerService } from './car-owner.service';
 import { CreateCarOwnerDto } from './dto/create-car-owner.dto';
 import { CarOwner } from './entities/car-owner.entity';
-import { Roles } from 'src/shared/decorators/roles.decorator';
-import { UserRole } from 'src/shared/enums';
+import { Roles } from 'src/common/decorators/roles.decorator';
+import { UserRole } from 'src/common/enums';
+import { RolesGuard } from 'src/auth/guards/roles.guard';
+import { ApiTags, ApiBearerAuth } from '@nestjs/swagger';
+import { API_TAGS } from 'src/common/enums';
+import { RequestService } from './services/request-service.service';
+import { CreateGeneralRepairDto } from './services/dtos/general-repair.dto';
+import { CreateMaintenanceRepairDto } from './services/dtos/maintenance-repair.dto';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
+import { MaintenanceRepair } from './services/entities/maintenance-repair.entity';
+import { GeneralRepair } from './services/entities/general-repair.entity';
 
+@ApiTags(API_TAGS.CAR_OWNER)
 @Controller('car-owners')
 export class CarOwnerController {
-  constructor(private readonly carOwnerService: CarOwnerService) {}
+  constructor(
+    private readonly carOwnerService: CarOwnerService,
+    private readonly requestService: RequestService,
+  ) {}
 
+  @UseGuards(RolesGuard)
   @Roles(UserRole.CAR_OWNER)
   @Post('create')
   async createCarOwner(
@@ -17,6 +31,34 @@ export class CarOwnerController {
   ): Promise<CarOwner> {
     return this.carOwnerService.createCarOwner(
       createCarOwnerDto,
+      registrationToken,
+    );
+  }
+
+  @Post('maintenance')
+  @ApiBearerAuth()
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles(UserRole.CAR_OWNER)
+  async createMaintenanceRepair(
+    @Headers('registration-token') registrationToken: string,
+    @Body() createMaintenanceRepairDto: CreateMaintenanceRepairDto,
+  ): Promise<MaintenanceRepair> {
+    return this.requestService.createMaintenanceRepair(
+      createMaintenanceRepairDto,
+      registrationToken,
+    );
+  }
+
+  @Post('general')
+  @ApiBearerAuth()
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles(UserRole.CAR_OWNER)
+  async createGeneralRepair(
+    @Headers('registration-token') registrationToken: string,
+    @Body() createGeneralRepairDto: CreateGeneralRepairDto,
+  ): Promise<GeneralRepair> {
+    return this.requestService.createGeneralRepair(
+      createGeneralRepairDto,
       registrationToken,
     );
   }
